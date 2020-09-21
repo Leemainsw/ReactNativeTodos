5f818f61bb1817ab7ca88a0bed4f409673305ff3
Leemainsw <s2018s15@e-mirim.hs.kr>
2020-09-21 15:52:56 +0900

    아이콘 추가하기

--- ReactNativeTodos/components/TodoListItem.js
+++ ReactNativeTodos/components/TodoListItem.js
@@ -1,14 +1,26 @@
 //components/TodoListItem.js
 import React from 'react';
+import Icon from 'react-native-vector-icons/AntDesign';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 const TodoListItem = () => {
-    return(
+    return (
         <View style={styles.container}>
             <TouchableOpacity>
-                <View style={styles.circle} />
+                <View style={styles.completeCircle}>
+                    <Text style={styles.buttonText}>
+                        <Icon theme={{ iconFamily: 'AntDesign' }} name='circledowno' size={30} color="#3143e8"/>
+                    </Text>
+                </View>
             </TouchableOpacity>
-            <Text style={styles.text}>TodoList items will be shown here</Text>
+            <Text style={[styles.text, styles.strikeText]}>
+                Items will be shown here
+            </Text>
+            <TouchableOpacity style={styles.buttonContainer}>
+                <Text style={styles.buttonText}>
+                    <Icon theme={{ iconFamily: 'AntDesign' }} name="delete" size={30} color="#e33057" />
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -37,6 +49,21 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         marginRight: 20,
         marginLeft: 20,
+    },
+    completeCircle: {
+        marginRight: 20,
+        marginLeft: 20,
+    },
+    strikeText: {
+        color: '#bbb',
+        textDecorationLine: 'line-through',
+    },
+    unstrikeText: {
+        color: '#29323c',
+    },
+    buttonContainer: {
+        marginVertical: 10,
+        marginHorizontal: 10,
     },
 });
 
